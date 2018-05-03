8c6f6e712e3917223568ca7c95eef02c6da13c95
Rafael Rojas <yugnarcorp@hotmail.com>
2018-05-03 02:02:11 -0500

    Adding peach

--- Mario.java
+++ Mario.java
@@ -2,7 +2,8 @@ public class Mario{
 	public static void main(String[] args) {
 		int mario = 3;
 		int luigi = 4;
+		int peach = 5;
 
-		System.out.println(mario + luigi);
+		System.out.println(mario + luigi - peach);
 	}
 }
